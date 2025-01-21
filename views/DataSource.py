62c049a1aee581c661bfe00db8544a1650556717
A-Kuburovic <124525687+A-Kuburovic@users.noreply.github.com>
2025-01-21 22:44:06 +0100

    Update DataSource.py

--- views/DataSource.py
+++ views/DataSource.py
@@ -2,14 +2,14 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 
 # LINK TIL GSHEET PÅ GOOGLE DREV
-url = "https://docs.google.com/spreadsheets/d/1oh4NIxUlJH8Y11OQV4GC_G3IWbaqD7FUfrVwUBTMGOE/edit?usp=sharing"
+#url = "https://docs.google.com/spreadsheets/d/1oh4NIxUlJH8Y11OQV4GC_G3IWbaqD7FUfrVwUBTMGOE/edit?usp=sharing"
 
 # FORBINDELSE TIL SHEET
-conn = st.connection("gsheets", type=GSheetsConnection)
+#conn = st.connection("gsheets", type=GSheetsConnection)
 
 # DANNELSE AF DATA
-data = conn.read(spreadsheet=url, worksheet="0")
-
+#data = conn.read(spreadsheet=url, worksheet="0")
+data = "Fantasy.xlsx"
 # TITEL
 st.title("Data Source")
 
